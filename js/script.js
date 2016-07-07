025b96863501d90e63c725ffe2cd628dcfaeeaa7
Maxim Topciu <maximtop@gmail.com>
2016-07-07 16:27:10 +0300

    Add location determination

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 /**
  * Created by maxim on 07.07.2016.
  */
+
+var key = '51e580c83a42e14ac0a593f7ae0814e9';
+
+function getTemp(metrics) {
+    
+}
+
+function dataByIp() {
+    return $.getJSON('http://ipinfo.io');
+}
+
+$(document).ready(function () {
+    dataByIp().done(function(ipData) {
+        $('#location').text(ipData.city + ', ' + ipData.country);
+    });
+});
