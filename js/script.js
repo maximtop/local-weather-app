c0f963a3c880a36f2b60ff66c62981d419608388
maxim <maximtop@gmail.com>
2016-07-09 14:12:57 +0300

    Add toggle between Fahrenheit and Celsius.

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@
  * Created by maxim on 07.07.2016.
  */
 
+const WEATHER_APP_KEY = '51e580c83a42e14ac0a593f7ae0814e9'; 
+
 $(document).ready(function () {
 
-    const WEATHER_APP_KEY = '51e580c83a42e14ac0a593f7ae0814e9';
 
     function dataByIp() {
         return $.getJSON('http://ipinfo.io');
@@ -17,21 +18,60 @@ $(document).ready(function () {
         var cityLat = locationArray[0];
         var cityLon = locationArray[1];
         console.log(cityLat, cityLon);
+
+        var tempUnits = 'metric';
+        var metrics = $("#metrics");
+
+        metrics.click(function () {
+            if (metrics.text() == "C") {
+                tempUnits = "imperial";
+                metrics.text("F");
+            } else {
+                tempUnits = "metric";
+                metrics.text("C");
+            }
+            getTemp(tempUnits);
+        });
+
+        function getTemp(tempUnits) {
+
+            var weatherApi = "http://api.openweathermap.org/data/2.5/weather";
+            console.log(tempUnits);
+            var data = {
+                lat : cityLat,
+                lon : cityLon,
+                units: tempUnits,
+                lang: 'ru',
+                appid : WEATHER_APP_KEY
+            };
+
+            $.getJSON(weatherApi, data, function(data) {
+                console.log(data);
+                console.log(data.main.temp);
+                console.log(data.weather[0].main);
+                $('#description').text(data.weather[0].main);
+                $('#temp').text(Math.round(data.main.temp));
+            })
+        }
+
+        getTemp(tempUnits);
+
     });
 
-    function getTemp() {
-        var weatherApi = "http://api.openweathermap.org/data/2.5/weather";
-        var data = {
-            lat : '55.7522',
-            lon : '37.6156',
-            units: 'metric',
-            appid : '51e580c83a42e14ac0a593f7ae0814e9'
-        };
-        $.getJSON(weatherApi, data, function(data) {
-            console.log(data.main.temp);
-            $('#temp').text(Math.round(data.main.temp));
-        })
-    }
-
-    getTemp();
+    // function getTemp() {
+    //     var weatherApi = "http://api.openweathermap.org/data/2.5/weather";
+    //     var data = {
+    //         lat : '55.7522',
+    //         lon : '37.6156',
+    //         units: 'metric',
+    //         appid : '51e580c83a42e14ac0a593f7ae0814e9'
+    //     };
+    //
+    //     $.getJSON(weatherApi, data, function(data) {
+    //         console.log(data.main.temp);
+    //         $('#temp').text(Math.round(data.main.temp));
+    //     })
+    // }
+    //
+    // getTemp();
 });
